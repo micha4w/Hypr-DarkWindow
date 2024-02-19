87ada2fa4715cc5bb5e938fe1b1460b79daa061a
micha4w <micha4w@gmail.com>
2024-02-19 16:54:44 +0100

    ci(nix): add flake.nix

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    hyprland.url = "github:hyprwm/Hyprland";
+    nix-filter.url = "github:numtide/nix-filter";
+  };
+
+  outputs = {
+    self,
+    hyprland,
+    nix-filter,
+    ...
+  }: let
+    inherit (hyprland.inputs) nixpkgs;
+    forHyprlandSystems = fn: nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (system: fn system nixpkgs.legacyPackages.${system});
+  in {
+    packages = forHyprlandSystems (system: pkgs: rec {
+      Hypr-DarkWindow = pkgs.gcc13Stdenv.mkDerivation {
+        pname = "Hypr-DarkWindow";
+        version = "0.1";
+        src = nix-filter.lib {
+          root = ./.;
+          include = [
+            "src"
+            ./Makefile
+          ];
+        };
+
+        # allow overriding xwayland support
+        BUILT_WITH_NOXWAYLAND = false;
+
+        nativeBuildInputs = with pkgs; [meson ninja pkg-config];
+
+        buildInputs = with pkgs;
+          [
+            hyprland.packages.${system}.hyprland.dev
+            pango
+            cairo
+          ]
+          ++ hyprland.packages.${system}.hyprland.buildInputs;
+
+        meta = with pkgs.lib; {
+          homepage = "https://github.com/micha4w/Hypr-DarkWindow";
+          description = "Invert the colors of specific Windows";
+          # license = licenses.bsd3;
+          platforms = platforms.linux;
+        };
+      };
+
+      default = Hypr-DarkWindow;
+    });
+
+    devShells = forHyprlandSystems (system: pkgs: {
+      default = pkgs.mkShell {
+        name = "Hypr-DarkWindow";
+
+        nativeBuildInputs = with pkgs; [
+          clang-tools_16
+          bear
+        ];
+
+        inputsFrom = [self.packages.${system}.Hypr-DarkWindow];
+      };
+    });
+  };
+}
