9d27b9d6829bc822287c39b66a5abfe61fdf17d6
micha4w <micha4w@gmail.com>
2024-05-14 00:03:40 +0200

    nix: fix flake submodule being stupid

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
     nix-filter.url = "github:numtide/nix-filter";
   };
