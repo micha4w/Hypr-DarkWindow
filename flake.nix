7bc0ca28f5e2a7d7dda30aecdbdd4688dbda3e3e
micha4w <micha4w@gmail.com>
2025-06-28 21:07:49 +0200

    feat: add support for custom shaders

--- flake.nix
+++ flake.nix
@@ -5,58 +5,72 @@
     nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = {
-    self,
-    hyprland,
-    nix-filter,
-    ...
-  }: let
-    inherit (hyprland.inputs) nixpkgs;
-    forHyprlandSystems = fn: nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (system: fn system nixpkgs.legacyPackages.${system});
-  in {
-    packages = forHyprlandSystems (system: pkgs: let
-      hyprlandPackage = hyprland.packages.${system}.hyprland;
-    in rec {
-      Hypr-DarkWindow = pkgs.gcc14Stdenv.mkDerivation {
-        pname = "Hypr-DarkWindow";
-        version = "3.0.1";
-        src = nix-filter.lib {
-          root = ./.;
-          include = [
-            "src"
-            ./Makefile
-          ];
-        };
+  outputs =
+    {
+      self,
+      hyprland,
+      nix-filter,
+      ...
+    }:
+    let
+      inherit (hyprland.inputs) nixpkgs;
+      forHyprlandSystems =
+        fn:
+        nixpkgs.lib.genAttrs (builtins.attrNames hyprland.packages) (
+          system: fn system nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      packages = forHyprlandSystems (
+        system: pkgs:
+        let
+          hyprlandPackage = hyprland.packages.${system}.hyprland;
+        in
+        rec {
+          Hypr-DarkWindow = pkgs.gcc14Stdenv.mkDerivation {
+            pname = "Hypr-DarkWindow";
+            version = "3.0.1";
+            src = nix-filter.lib {
+              root = ./.;
+              include = [
+                "src"
+                ./Makefile
+              ];
+            };
 
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-        buildInputs = [hyprlandPackage.dev] ++ hyprlandPackage.buildInputs;
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            buildInputs = [ hyprlandPackage.dev ] ++ hyprlandPackage.buildInputs;
 
-        installPhase = ''
-          mkdir -p $out/lib
-          install ./out/hypr-darkwindow.so $out/lib/libHypr-DarkWindow.so
-        '';
+            installPhase = ''
+              mkdir -p $out/lib
+              install ./out/hypr-darkwindow.so $out/lib/libHypr-DarkWindow.so
+            '';
 
-        meta = with pkgs.lib; {
-          homepage = "https://github.com/micha4w/Hypr-DarkWindow";
-          description = "Invert the colors of specific Windows";
-          license = licenses.mit;
-          platforms = platforms.linux;
-        };
-      };
+            meta = with pkgs.lib; {
+              homepage = "https://github.com/micha4w/Hypr-DarkWindow";
+              description = "Invert the colors of specific Windows";
+              license = licenses.mit;
+              platforms = platforms.linux;
+            };
+          };
 
-      default = Hypr-DarkWindow;
-    });
+          default = Hypr-DarkWindow;
+        }
+      );
 
-    devShells = forHyprlandSystems (system: pkgs: {
-      default = pkgs.mkShell {
-        name = "Hypr-DarkWindow";
+      devShells = forHyprlandSystems (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            name = "Hypr-DarkWindow";
 
-        nativeBuildInputs = with pkgs; [
-          clang-tools_16
-        ];
+            nativeBuildInputs = with pkgs; [
+              clang-tools_16
+              jq
+            ];
 
-        inputsFrom = [self.packages.${system}.Hypr-DarkWindow];
-      };
-    });
-  };
+            inputsFrom = [ self.packages.${system}.Hypr-DarkWindow ];
+          };
+        }
+      );
+    };
 }
