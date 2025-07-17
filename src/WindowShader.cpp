4fec5fad16898ecad0776b460dfff41591afba65
alexhulbert <alex@alexhulbert.com>
2025-07-17 12:59:49 -0400

    feat: chromakey shader

--- src/WindowShader.cpp
+++ src/WindowShader.cpp
@@ -24,6 +24,30 @@ static const std::map<std::string, std::pair<std::string, Uniforms>> WINDOW_SHAD
         { "tintColor", { 1, 0, 0 } },
         { "tintStrength", { 0.1 } },
     }} },
+    // Original shader by ikz87
+    // Applies opacity changes to pixels similar to one color
+    { "chromakey", { R"glsl(
+        uniform vec3 bkg;
+        uniform float similarity; // How many similar colors should be affected.
+        uniform float amount; // How much similar colors should be changed.
+        uniform float targetOpacity; // Target opacity for similar colors.
+
+        void windowShader(inout vec4 color) {
+            if (color.r >= bkg.r - similarity && color.r <= bkg.r + similarity &&
+                    color.g >= bkg.g - similarity && color.g <= bkg.g + similarity &&
+                    color.b >= bkg.b - similarity && color.b <= bkg.b + similarity &&
+                    color.a >= 0.99) {
+                vec3 error = vec3(abs(bkg.r - color.r), abs(bkg.g - color.g), abs(bkg.b - color.b));
+                float avg_error = (error.r + error.g + error.b) / 3.0;
+                color.a = targetOpacity + (1.0 - targetOpacity) * avg_error * amount / similarity;
+            }
+        }
+    )glsl", {
+        { "bkg", { 0, 0, 0 } },
+        { "similarity", { 0.1 } },
+        { "amount", { 1.4 } },
+        { "targetOpacity", { 0.83 } },
+    }} },
 };
 
 
